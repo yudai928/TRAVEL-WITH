49ea2472e77295d3715836a3a516c9a4522022ff
yudai928 <82750792+yudai928@users.noreply.github.com>
2021-06-14 14:17:51 +0900

    自動デプロイの設定

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+lock '3.16.0'
+
+set :application, ‘find-japan'
+
+set :repo_url,  'git@github.com:yudai928/TRAVEL-WITH.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5' 
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/yudai928.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
