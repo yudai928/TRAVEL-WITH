88d2e0c3c055afcc935c797ee247578261c1b4a5
yudai928 <82750792+yudai928@users.noreply.github.com>
2021-06-14 21:20:15 +0900

    ActionCable本番環境

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,7 @@ Rails.application.routes.draw do
   resources :choices, only: [:index]
   resources :locations, only: [:index, :show]
   resources :chat_rooms, only: [:create, :show]
+
+  mount ActionCable.server => '/cable'
+  
 end
