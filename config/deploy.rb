1258407ebb1ebe03afd5d9ab51611d68689890f8
yudai928 <82750792+yudai928@users.noreply.github.com>
2021-06-16 15:41:44 +0900

    Revert "自動デプロイ設定"
    This reverts commit 7a46412cf4671dd3eb8e364deb8592435471788c.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,16 @@
 lock '3.16.0'
 
-set :application, 'find-japan'
+set :application, ‘find-japan'
 
 set :repo_url,  'git@github.com:yudai928/TRAVEL-WITH.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.6.5'
+set :rbenv_ruby, '2.6.5' 
 
 set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/yudai.pem'] 
+                                  keys: ['~/.ssh/yudai928.pem'] 
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
